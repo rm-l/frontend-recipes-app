fbe1bc5f27cc792e671e90048bf3736381e9d194
João Victor Schiavon <schiavonj@yahoo.com.br>
2022-10-27 13:02:57 -0300

    FEAT: requisito 19

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import AppContext from '../context/AppContext';
+import Recipes from '../components/Recipes';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 function Meals() {
+  const { setMealsList, setIsMultipleMeals, setPath, mealsList, isInitialMeals,
+    setIsInitialMeals } = useContext(AppContext);
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setPath(pathname);
+    const fetchInitialApi = async () => {
+      const TWELVE = 12;
+      const endPoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+      const response = await fetch(endPoint);
+      const { meals } = await response.json();
+      const filteredMeals = meals.filter((item, index) => index < TWELVE);
+      setMealsList(filteredMeals);
+      setIsMultipleMeals(false);
+      setIsInitialMeals(true);
+    };
+    fetchInitialApi();
+  }, []);
+
   return (
     <div>
       <Header />
-
+      <div>
+        {
+          (isInitialMeals) && (
+            mealsList.map((meal, indexM) => (
+              <div key={ meal.strMeal } data-testid={ `${indexM}-recipe-card` }>
+                <Recipes
+                  meal={ meal }
+                  indexM={ indexM }
+                />
+              </div>
+            )))
+        }
+      </div>
       <Footer />
-
     </div>
 
   );
