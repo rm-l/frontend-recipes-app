70591a639163b861da404d731cb0845a35b7c259
Robson Silva <rm8l@outlook.com>
2022-10-28 20:07:50 -0300

    Requisito 25 em andamento

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -15,32 +15,72 @@ function RecipeDetails() {
   }
   const id = pathnameId();
 
-  const fetchApi = async () => {
-    if (pathname.includes('meals')) {
-      const response = await fetch((`${MEALS_ENDPOINT}${id}`));
-      const result = await response.json();
-      setRecipe(result.meals[0]);
-    }
-    (pathname.includes('drinks')); {
-      const response = await fetch((`${DRINK_ENDPOINT}${id}`));
-      const result = await response.json();
-      setRecipe(result.drinks[0]);
-    }
-  };
-
   useEffect(() => {
+    const fetchApi = async () => {
+      if (pathname === `/meals/${id}`) {
+        const response = await fetch((`${MEALS_ENDPOINT}${id}`));
+        const result = await response.json();
+        setRecipe(result.meals);
+      }
+      if (pathname === `/drinks/${id}`) {
+        const response = await fetch((`${DRINK_ENDPOINT}${id}`));
+        const result = await response.json();
+        setRecipe(result.drinks);
+      }
+    };
     fetchApi();
-  }, []);
+  }, [id, pathname]);
 
   return (
-    <>
-      <div data-testid="recipe-details" />
-      <span>
-        test
-        {console.log(recipe)}
-      </span>
-    </>
+    <div data-testid="recipe-details">
+      {
+        (pathname === `/meals/${id}`) && (
+          <div>
+            {console.log(recipe)}
+            {recipe?.map((meal) => (
+              <div key={ meal.strMeal }>
+                <h1 data-testid="recipe-title">
+                  {meal.strMeal}
+                </h1>
+                <span data-testid="recipe-category">
+                  {meal.strCategory}
+                </span>
+                <img
+                  src={ meal.strMealThumb }
+                  alt={ meal.strMeal }
+                  data-testid="recipe-photo"
+                />
+                {/* <span data-testid={ `${i}-ingredient-name-and-measure` }>
+                  {
+                    `
+                    ${meal.strIngredient1} -
+                    ${meal.strMeasure1}`
+                  }
+                  <br />
+                </span> */}
+                <p data-testid="instructions">{meal.strInstructions}</p>
 
+              </div>))}
+          </div>
+        )
+      }
+      {
+        (pathname === `/drinks/${id}`)
+           && (
+             <div>
+               {console.log(recipe.strDrink)}
+
+               {/* <h1>{recipe.strDrink}</h1> */}
+               { /* <p data-testid={ `${indexD}-card-name` }>{drink.strDrink}</p>
+               <img
+                 src={ drink.strDrinkThumb }
+                 alt={ drink.strDrink }
+                 data-testid={ `${indexD}-card-img` }
+               /> */}
+             </div>
+           )
+      }
+    </div>
   );
 }
 
